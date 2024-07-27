3c67a9782af8f06ed55c9d8483fc3950d2bdf3f6
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-07-27 15:22:07 +0530

    Update paralleltransfer.py

--- tgfilestream/paralleltransfer.py
+++ tgfilestream/paralleltransfer.py
@@ -1,18 +1,3 @@
-# tgfilestream - A Telegram bot that can stream Telegram files to users over HTTP.
-# Copyright (C) 2019 Tulir Asokan
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from typing import Union, AsyncGenerator, AsyncContextManager, Dict, Optional, List
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
@@ -67,7 +52,7 @@ class DCConnectionManager:
         self.dc_id = dc_id
         self.auth_key = None
         self.connections = []
-        self._list_lock = asyncio.Lock()
+        self._list_lock = asyncio.Lock()  # Remove 'loop=self.loop'
         self.loop = client.loop
         self.dc = None
 
@@ -76,7 +61,7 @@ class DCConnectionManager:
             self.dc = await self.client._get_dc(self.dc_id)
         sender = MTProtoSender(self.auth_key, self.loop, loggers=self.client._log)
         index = len(self.connections) + 1
-        conn = Connection(sender=sender, log=self.log.getChild(f"conn{index}"), lock=asyncio.Lock())
+        conn = Connection(sender=sender, log=self.log.getChild(f"conn{index}"), lock=asyncio.Lock())  # Remove 'loop=self.loop'
         self.connections.append(conn)
         async with conn.lock:
             conn.log.info("Connecting...")
